370f4e7eb61731a8af6bb95aa8665d39cc91f143
Koda <gasparvarela98@gmail.com>
2024-06-01 15:30:07 -0400

    Made code more legible and moved functions from main to server

--- server.py
+++ server.py
@@ -2,10 +2,18 @@ from http.server import SimpleHTTPRequestHandler, HTTPServer
 import os
 from filetransfer import FileUploadHandler
 
+#Only for default inputs if user doesn't want to specify port or ip            
+def defaultInput( prompt, default ):
+    x = input(prompt)
+    if not x.strip():
+        x = default
+    return x
+
 class Server:
-    def __init__(self, ip=None, port = 8000):
+    def __init__(self, port = None, ip=None):
         self.ip = ip
-        self.port = port       
+        self.port = port
+        self.f = False     
 
     def run(self):
         server_class = HTTPServer
@@ -17,3 +25,39 @@ class Server:
     def makedir(self, name):
         os.makedirs(name, exist_ok=True)
         print('Path {} created succesfully!'.format(name))
+    
+    def start(self):
+        try:
+            try:
+                defaultIp = "127.0.0.1"
+                defaultPort = "8000"
+                self.ip = str(defaultInput("Specify server ip, press enter for default (localhost)\n", defaultIp))
+                self.port = int(defaultInput("Specify server port to listen to, press enter for default (8000)\n", defaultPort))
+            except TypeError:
+                print("A port and ip number is needed to run the server, please make sure to use only integers on port or string on ip")
+            except EOFError:
+                print("Port or ip number can't be blank")
+            else:
+                print("Server succesfully started on port {}".format(self.port))
+                self.makedir("uploads")
+                self.setHTMLport(self.ip + ':' + str(self.port))
+                self.run()
+        except KeyboardInterrupt:
+                print("Server stopped succesfully from KeyboardInterrupt")
+        finally:
+                self.setHTMLport("{url}")
+                print("Server stopped listening to port {}".format(self.port))
+              
+    def setHTMLport(self, x):
+        with open("upload.html", "r", encoding="utf-8") as file:
+            html_content = file.read()
+        
+        if (self.f == False):
+            html_content = html_content.replace("{url}", x)
+            self.f = True
+        else:
+            html_content = html_content.replace("{}".format(self.ip + ':' + str(self.port)), x)
+            self.f = False
+
+        with open("upload.html", "w", encoding="utf-8") as file:
+            file.write(html_content)       
